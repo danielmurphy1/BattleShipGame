03ca3e2b5ab52075555f9f1be05826d14f2754f1
danielmurphy1 <daniel.murphy.1@gmail.com>
2021-12-14 22:26:50 -0500

    changed gameBoard and update logic so that x coords can be displayed at bottom and y coords number bottom to top least to greatest

--- BattleShipGame/Program.cs
+++ BattleShipGame/Program.cs
@@ -12,6 +12,7 @@ namespace BattleShipGame
 
             bool isBattleShipSunk = false;
 
+
             while(!isBattleShipSunk)
             {
                 Console.Write("Enter guess for X (horizontal) coordinate (column) 1-10: ");
@@ -23,9 +24,9 @@ namespace BattleShipGame
                 Console.Write("Enter guess for Y (vertical) coordinate (row) 1-10: ");
                 if (int.TryParse(Console.ReadLine(), out int yCoordValue))
                 {
-                    player.yChoice = yCoordValue;
+                    player.yChoice = 10 - yCoordValue;
                 }
-                Console.WriteLine($"Your guess was {player.xChoice}, {player.yChoice}");
+                Console.WriteLine($"Your guess was {player.xChoice}, {10 - player.yChoice}");
                 screen.UpdateGameBoard(player.yChoice, player.xChoice, " X");
             }
 
